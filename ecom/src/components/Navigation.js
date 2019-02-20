aa8aa99c76e35ac4d2b37bbb4cacc2de35dd057b
Chiboba <briangfelix@gmail.com>
2019-02-19 19:00:50 -0800

    Nav Styled

--- ecom/src/components/Navigation.js
+++ ecom/src/components/Navigation.js
@@ -1,35 +1,47 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom'
+import './Nav.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart, faSearch, faUser } from '@fortawesome/free-solid-svg-icons'
+
 
 const Post= props => { 
     return (
-        <div className = 'navigation'>
-            <img/> {/*LOGO*/}
-            <nav>
-                <Link to='/collections/skincare'>
-                    SKINCARE
-                </Link>
-                <Link to='/pages/routine'>
-                    ROUTINE
-                </Link>
-                <Link to='/collections/best-sellers'>
-                    BEST SELLERS
-                </Link>
-                <Link to='/pages/brands'>
-                    BRANDS
-                </Link>
-                <Link to='/pages/get-started'>
-                    GET STARTED
-                </Link>
-                <Link to='/pages/rewards'>
-                    REWARDS
-                </Link>
-                <Link to='/collections/sale'>
-                    SALE
-                </Link>
-            </nav>
-            {/* Profile Component*/}
-            {/* Cart Component */}
-            {/* SearchBar Component */}
+        <div className="extendnav">
+            <div className = 'navigation'>
+                <img src = "https://theordinary.com/Images/logo.svg" alt="logo"/>
+                <nav>
+                    <NavLink to='/collections/skincare'>
+                        SKINCARE
+                    </NavLink>
+                    <NavLink to='/pages/routine'>
+                        ROUTINE
+                    </NavLink>
+                    <NavLink to='/collections/best-sellers'>
+                        BEST SELLERS
+                    </NavLink>
+                    <NavLink to='/pages/brands'>
+                        BRANDS
+                    </NavLink>
+                    <NavLink to='/pages/get-started'>
+                        GET STARTED
+                    </NavLink>
+                    <NavLink to='/pages/rewards'>
+                        REWARDS
+                    </NavLink>
+                    <NavLink to='/collections/sale'>
+                        SALE
+                    </NavLink>
+                </nav>
+                <div className="navicons">
+                    <FontAwesomeIcon  icon={faShoppingCart} size={70}/>
+                    <FontAwesomeIcon  icon={faUser} />
+                    <FontAwesomeIcon  icon={faSearch} />
+                </div>
+                {/* Profile Component*/}
+                {/* Cart Component */}
+                {/* SearchBar Component */}
+            </div>
         </div>
     );
 }
