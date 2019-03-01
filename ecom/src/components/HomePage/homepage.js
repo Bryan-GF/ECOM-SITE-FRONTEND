c3f3ab90b8869e3f42030f4f2c1e9b57eda641ff
Chiboba <briangfelix@gmail.com>
2019-02-28 16:06:49 -0800

    Working product tab selection, bug by arrow and side scrolling

--- ecom/src/components/HomePage/homepage.js
+++ ecom/src/components/HomePage/homepage.js
@@ -7,14 +7,28 @@ class HomePage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        
+        filterType: "type"
     };
   }
+
+  setType = (ev) => {
+    this.setState({filterType: ev.target.name})
+    console.log(ev.target.name);
+  }
+
   render() {
     return (
       <div className="Homepage-Wrapper">
         <Carousel/>
-        <ProductCarousel/>
+        <div className="sctype-wrapper">
+          <div className="sctype-list">
+            <button name ="type" onClick={this.setType}>SHOP BY SKIN TYPE</button>
+            <button name ="concern" onClick={this.setType}>SHOP BY SKIN CONCERN</button>
+            <button name ="routine" onClick={this.setType}>SHOP BY ROUTINE</button>
+            <button name ="bestseller" onClick={this.setType}>SHOP BY BEST SELLERS</button>
+          </div> 
+          <ProductCarousel type={this.state.filterType}/>
+        </div>
       </div>
     );
   }
