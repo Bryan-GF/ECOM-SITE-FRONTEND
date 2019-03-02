73000573434cf6189fcd27de9200829f4c803409
Chiboba <briangfelix@gmail.com>
2019-03-01 17:41:36 -0800

    Mostly functioning product carousel, issue with differing lenths

--- ecom/src/components/HomePage/homepage.js
+++ ecom/src/components/HomePage/homepage.js
@@ -7,12 +7,18 @@ class HomePage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        filterType: "type"
+        filterType: "type",
+        imagesObject: {
+          "type": ["https://divinotes.com/wp-content/uploads/2017/08/Free-WordPress-Logo-Carousel-hero-1280x640.jpg", "https://images.pexels.com/photos/257360/pexels-photo-257360.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", '//cdn.shopify.com/s/files/1/0249/1218/files/calming_and_healing_mobile_v1_782x.png?v=1549050776',
+          "https://images.pexels.com/photos/257840/pexels-photo-257840.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", "https://images.pexels.com/photos/206673/pexels-photo-206673.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", "https://images.pexels.com/photos/33109/fall-autumn-red-season.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"],
+          "concern": ["https://images.pexels.com/photos/257840/pexels-photo-257840.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", "https://images.pexels.com/photos/206673/pexels-photo-206673.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", "https://images.pexels.com/photos/33109/fall-autumn-red-season.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        "https://images.pexels.com/photos/459301/pexels-photo-459301.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", "https://images.pexels.com/photos/66874/landscape-meadow-field-mountains-66874.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"]
+        }
     };
   }
 
   setType = (ev) => {
-    this.setState({filterType: ev.target.name})
+    this.setState({filterType: ev.target.name, transVal: 0})
     console.log(ev.target.name);
   }
 
@@ -25,9 +31,9 @@ class HomePage extends Component {
             <button name ="type" onClick={this.setType}>SHOP BY SKIN TYPE</button>
             <button name ="concern" onClick={this.setType}>SHOP BY SKIN CONCERN</button>
             <button name ="routine" onClick={this.setType}>SHOP BY ROUTINE</button>
-            <button name ="bestseller" onClick={this.setType}>SHOP BY BEST SELLERS</button>
+            <button name ="bestseller" onClick={this.setType} >SHOP BY BEST SELLERS</button>
           </div> 
-          <ProductCarousel type={this.state.filterType}/>
+          <ProductCarousel images={this.state.imagesObject[this.state.filterType]}/>
         </div>
       </div>
     );
